b75d6833f6f991a561b734b6006882f8162f2049
Michael Cervantes <52178+emcee21@users.noreply.github.com>
2025-03-21 09:06:32 -0700

    Add set_env.sh script to retrieve task metadata and configure environment variables #9

---
+++ set_env.sh
@@ -0,0 +1,14 @@
+
+# Use IMDSv2 to get current AZ
+
+TASK_METADATA=$(curl -s ${ECS_CONTAINER_METADATA_URI_V4}/task)
+echo "Task Metadata: $TASK_METADATA"
+
+AZ=$(echo $TASK_METADATA | jq -r '.AvailabilityZone')
+echo "Current Availability Zone: $AZ"
+
+export MAIL_HOST="${SMTP_HOSTNAME}.${AZ}.${WORKSPACE}.${DOMAIN}"
+export HTTP_PROXY="http://${PROXY_HOSTNAME}.${AZ}.${WORKSPACE}.${DOMAIN}:${PROXY_PORT}"
+
+echo "SMTP_HOST: $SMTP_HOST"
+echo "HTTP_PROXY: $HTTP_PROXY"
